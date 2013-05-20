577c1a60fe4a5f36053501db8aa79551260cda46
Alvaro de Miguel <adarrivi@gmail.com>
2013-05-20 07:29:03 +0100

    New project to load sample data

---
+++ data-loader/src/main/java/com/hidden/data/loader/BookDao.java
@@ -0,0 +1,18 @@
+package com.hidden.data.loader;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookDao {
+
+	private String property;
+
+	public void doSomething() {
+		System.out.println("Something done: " + property);
+	}
+
+	public void setProperty(String property) {
+		this.property = property;
+	}
+
+}
---
+++ data-loader/src/main/java/com/hidden/data/loader/SpringApplicationContextExample.java
@@ -0,0 +1,26 @@
+package com.hidden.data.loader;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SpringApplicationContextExample {
+
+	public static void main(String[] args) {
+		ApplicationContext ctx = new ClassPathXmlApplicationContext(
+				"applicationContext.xml");
+		SpringApplicationContextExample example = ctx
+				.getBean(SpringApplicationContextExample.class);
+		example.start();
+	}
+
+	@Autowired
+	private BookDao bookDao;
+
+	private void start() {
+		bookDao.doSomething();
+	}
+
+}
