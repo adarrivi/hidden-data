d9c33c870553f20dfc44fa8c7f0dc12e41f2d6a0
Alvaro de Miguel <adarrivi@gmail.com>
2013-05-25 21:33:59 +0100

    Trying Spring embedded databases. Testing with Liquibase

---
+++ hidden-data-db/src/main/resources/database/dbCreation.sql
@@ -0,0 +1,19 @@
+DROP TABLE IF EXISTS book;
+DROP TABLE IF EXISTS author;
+
+CREATE TABLE author (
+    id INT(10) NOT NULL AUTO_INCREMENT,
+    name VARCHAR(250) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+
+CREATE TABLE book (
+    id INT(10) NOT NULL  AUTO_INCREMENT,
+    title VARCHAR(250) NOT NULL,
+    authorId INT(10) NOT NULL,
+    content MEDIUMBLOB NOT NULL,
+    PRIMARY KEY (id),
+    INDEX book_author (authorId),
+    CONSTRAINT book_author FOREIGN KEY (authorId) REFERENCES author (id)
+);
