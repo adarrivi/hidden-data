ff2d6527f685d8f3eddadd00c4bf01ebb607d0c7
Alvaro de Miguel <adarrivi@gmail.com>
2013-08-08 07:19:29 +0100

    Moving logic to Service

---
+++ data-loader/src/main/java/com/hidden/data/loader/service/BookService.java
@@ -0,0 +1,17 @@
+package com.hidden.data.loader.service;
+
+import com.common.performance.PerformanceLogged;
+import com.hidden.data.db.model.Author;
+import com.hidden.data.loader.BookFile;
+
+public interface BookService {
+
+	@PerformanceLogged(identifier = "SaveBook")
+	void saveBook(BookFile bookFile, Author author);
+
+	@PerformanceLogged(identifier = "SaveBookIfDoesntExist")
+	// This is not working because BookServiceImpl is not getting proxied. Add
+	// transaction manager or other alternative?
+	void saveBookIfDoesntExist(BookFile bookFile, Author author);
+
+}
